8da624251b20cf601174eb1dd41a6be602411c16
Kostas Ziovas <k_ziovas@yahoo.gr>
2021-09-24 01:14:27 +0300

    Code file part1

---
+++ VioPliroforikh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VioPliroforikh.Startup))]
+namespace VioPliroforikh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
